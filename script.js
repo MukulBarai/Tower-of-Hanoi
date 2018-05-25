38c6b8f1885656949d3a71618ce2be690048c326
Mukul Barai <mukulbarai82@gmail.com>
2018-05-25 16:00:44 +0600

    created primary classes and functions

--- script.js
+++ script.js
@@ -6,5 +6,41 @@ canvas.width = canvasWidth;
 canvas.height = canvasHeight;
 
 
-context.fillStyle = 'red';
-context.fillRect(0, 0, canvasWidth, canvasHeight);
+function run(){
+  setInterval(function(){
+    update();
+    draw();
+  }, 500);
+}
+
+function update(){
+
+}
+
+function draw(){
+  context.fillStyle = 'red';
+  context.fillRect(0, 0, canvasWidth, canvasHeight);
+}
+
+function Pile(index, width, height){
+  this.width = width;
+  this.height = height;
+  this.xcor = index * (canvasWidth / 3) - width / 3;
+  this.ycor = 50;
+
+  this.draw = function(){
+    context.fillStyle = 'blue';
+    context.fillRect(this.xcor, this.ycor, this.width, this.height);
+  }
+}
+
+function Disc(width, height, color){
+  this.width = width;
+  this.height = height;
+  this.color = color;
+
+  this.draw = function(xcor, ycor){
+    context.fillStyle = color;
+    context.fillRect(xcor, ycor, this.width, this.height)
+  }
+}
